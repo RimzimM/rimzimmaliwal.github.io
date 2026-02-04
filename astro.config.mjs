e31ef345cebda9b080464ae6d95392c0e26e6e63
Rimzim <rimzimmaliwal08@gmail.com>
2026-02-03 17:13:27 -0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/',
+  base: '/rimzimmaliwal.github.io/',
   vite: {
     plugins: [tailwindcss()],
   },
